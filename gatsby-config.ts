eb77bb443aa614911968502dde113987a4339637
jgjgill <79239852+jgjgill@users.noreply.github.com>
2023-03-11 10:49:13 +0900

    chore: 프로젝트 세팅 eslint, prettier 설정 관련 라이브러리 설치

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,4 +1,4 @@
-import type { GatsbyConfig } from "gatsby"
+import type { GatsbyConfig } from 'gatsby'
 
 const config: GatsbyConfig = {
   siteMetadata: {
@@ -8,7 +8,19 @@ const config: GatsbyConfig = {
   // If you use VSCode you can also use the GraphQL plugin
   // Learn more at: https://gatsby.dev/graphql-typegen
   graphqlTypegen: true,
-  plugins: [],
+  plugins: [
+    'gatsby-plugin-image',
+    'gatsby-plugin-sharp',
+    'gatsby-transformer-sharp',
+    'gatsby-plugin-emotion',
+    {
+      resolve: `gatsby-source-filesystem`,
+      options: {
+        name: `contents`,
+        path: `${__dirname}/contents/`,
+      },
+    },
+  ],
 }
 
 export default config
