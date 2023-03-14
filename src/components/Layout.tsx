d0febe127357628d8ecd58b613a114077141f1d7
jgjgill <79239852+jgjgill@users.noreply.github.com>
2023-03-15 01:35:16 +0900

    style: Header 스타일 수정

--- src/components/Layout.tsx
+++ src/components/Layout.tsx
@@ -3,15 +3,16 @@ import { PATH } from 'constants/path'
 import * as React from 'react'
 import { StrictPropsWithChildren } from 'types/custom'
 
+import Flex from './@shared/Flex'
 import Link from './@shared/Link'
 
 const Layout = ({ children }: StrictPropsWithChildren) => {
   return (
     <Container>
       <Header>
-        <div>
+        <Flex justifyContent="flex-start" alignItems="center">
           <StyledLink to={PATH.HOME}>jgjgill</StyledLink>
-        </div>
+        </Flex>
       </Header>
 
       <Main>{children}</Main>
@@ -27,25 +28,19 @@ const Container = styled.div`
 
 const Header = styled.header`
   display: flex;
-  align-items: center;
   position: sticky;
   height: 60px;
-  padding: 20px;
   top: 0px;
+  padding: 0 40px;
   backdrop-filter: saturate(200%) blur(1ex);
   background: linear-gradient(to top, #d8b4fe 0%, #f0abfc 100%);
-
-  div {
-    width: 768px;
-    margin: 0 auto;
-  }
-`
-const Main = styled.main`
-  max-width: 768px;
-  margin: 0 auto;
-  padding: 20px;
+  width: 100%;
 `
 const StyledLink = styled(Link)`
   font-size: ${({ theme }) => theme.fontSize.xxl};
   font-weight: ${({ theme }) => theme.fontWeight.bold};
+`
+
+const Main = styled.main`
+  padding: 20px 40px;
 `
