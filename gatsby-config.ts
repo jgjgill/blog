3e5aa7ea1eba04fb230ceea7fefbe92405bfb1c7
jgjgill <79239852+jgjgill@users.noreply.github.com>
2023-03-20 20:36:07 +0900

    feat: 구글 관련 작업 gatsby-plugin-google-gtag 플러그인 추가 .env 설정

--- gatsby-config.ts
+++ gatsby-config.ts
@@ -1,5 +1,9 @@
 import type { GatsbyConfig } from 'gatsby'
 
+require('dotenv').config({
+  path: `.env.${process.env.NODE_ENV}`,
+})
+
 const config: GatsbyConfig = {
   siteMetadata: {
     siteUrl: `https://jgjgill-blog.netlify.app/`,
@@ -14,6 +18,15 @@ const config: GatsbyConfig = {
     'gatsby-plugin-sharp',
     'gatsby-transformer-sharp',
     'gatsby-plugin-emotion',
+    {
+      resolve: 'gatsby-plugin-google-gtag',
+      options: {
+        trackingIds: [process.env.GA_TRACKING_ID],
+        pluginConfig: {
+          head: true,
+        },
+      },
+    },
     {
       resolve: 'gatsby-plugin-mdx',
       options: {
