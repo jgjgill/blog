bf739178c7186960388f27546cdf0db786878815
jgjgill <dbdltm22@naver.com>
2023-12-08 14:47:52 +0900

    chore: seo 설명 텍스트 변경

--- src/components/Seo.tsx
+++ src/components/Seo.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 const Seo = ({
   title = 'jgjgill',
-  description = '프론트엔드 개발자 이종길입니다. 작은 것부터 한 걸음씩 나아가며 문제를 접근하는 것을 좋아합니다. 주변을 먼저 살피며 팀과 함께 성장하는 것을 지향합니다.',
+  description = '프론트엔드 개발자 이종길입니다. - Go High',
   siteUrl = 'https://jgjgill-blog.netlify.app/',
   ogImage = 'https://raw.githubusercontent.com/jgjgill/blog/main/src/images/og-image.png',
 }: Props) => {
