76357b2432cc0ecfd2a191622dfd3f4ef8ab1981
jgjgill <dbdltm22@naver.com>
2025-02-17 00:19:25 +0900

    fix: 임시 No Filled 구성

--- src/components/GoogleAdsense.tsx
+++ src/components/GoogleAdsense.tsx
@@ -1,4 +1,5 @@
-import React, { useEffect, useState } from 'react'
+import styled from '@emotion/styled'
+import React, { useEffect, useRef } from 'react'
 
 declare global {
   interface Window {
@@ -12,39 +13,65 @@ interface GoogleAdsenseProps {
   height?: string
 }
 
-const GoogleAdsense = ({ slot, width = '728x', height = '90px' }: GoogleAdsenseProps) => {
-  const [isClient, setIsClient] = useState(false)
+const GoogleAdsense = ({
+  slot,
+  width = '728px',
+  height = '90px',
+}: GoogleAdsenseProps) => {
+  const advertRef = useRef<HTMLModElement>(null)
 
   useEffect(() => {
-    setIsClient(true)
+    // AdSense 스크립트 로드
+    const script = document.createElement('script')
+    script.src =
+      'https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2173037699636228'
+    script.async = true
+    script.crossOrigin = 'anonymous'
+    document.head.appendChild(script)
 
-    try {
-      ;(window.adsbygoogle = window.adsbygoogle || []).push({})
-    } catch (error) {
-      console.error('AdSense 초기화 중 에러 발생:', error)
+    // 광고가 이미 로드되었는지 확인
+    const alreadyLoaded = advertRef.current?.getAttribute('data-ad-status')
+
+    if (!alreadyLoaded) {
+      try {
+        ;(window.adsbygoogle = window.adsbygoogle || []).push({})
+      } catch (error) {
+        console.error('AdSense 초기화 중 에러 발생:', error)
+      }
+    }
+
+    return () => {
+      // 컴포넌트 언마운트 시 스크립트 제거
+      document.head.removeChild(script)
     }
   }, [])
 
-  if (!isClient) {
-    return <div style={{ width, height }}>No Filled</div>
-  }
-
   return (
-    <>
-      <script
-        async
-        src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-2173037699636228"
-        crossOrigin="anonymous"
-      />
+    <AdContainer height={height} width={width}>
       <ins
+        ref={advertRef}
         className="adsbygoogle"
-        style={{ display: 'inline-block', width, height }}
+        style={{ display: 'inline-block', width, height, textDecoration: 'none' }}
         data-ad-client="ca-pub-2173037699636228"
         data-ad-slot={slot}
-      />
-      <script>{`(adsbygoogle = window.adsbygoogle || []).push({});`}</script>
-    </>
+      >
+        <span>No Filled</span>
+      </ins>
+    </AdContainer>
   )
 }
 
 export default GoogleAdsense
+
+const AdContainer = styled.div<{ width: string; height: string }>`
+  width: ${(props) => props.width};
+  height: ${(props) => props.height};
+
+  .adsbygoogle span {
+    display: none !important;
+  }
+
+  .adsbygoogle[data-ad-status='unfilled'] span {
+    display: block !important;
+  }
+`
